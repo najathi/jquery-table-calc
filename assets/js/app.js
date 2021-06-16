bfb59cf2538a34e3955bd7714689b01b2aefbd90
najathi <najathi777@gmail.com>
2021-06-17 01:52:05 +0530

    setup project

---
+++ assets/js/app.js
@@ -0,0 +1,720 @@
+const tableCartData = [
+    {
+        id: 1,
+        model: 'iPad mini 4',
+        category: 1,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: null,
+                amount: 40.00
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 80.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 15.00
+            },
+        ]
+    },
+    {
+        id: 2,
+        model: 'iPad mini 5',
+        category: 1,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: null,
+                amount: 50.00
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 100.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 20.00
+            },
+        ]
+    },
+    {
+        id: 3,
+        model: 'iPad Air 2',
+        category: 2,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: null,
+                amount: 40.00
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 80.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 15.00
+            },
+        ]
+    },
+    {
+        id: 4,
+        model: 'iPad Air 3',
+        category: 2,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: null,
+                amount: 40.00
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 80.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 15.00
+            },
+        ]
+    },
+    {
+        id: 5,
+        model: 'iPad Air 4',
+        category: 2,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: [
+                    {
+                        title: 'GOOD TOUCH',
+                        amount: 140.00
+                    },
+                    {
+                        title: 'FAULTY TOUCH',
+                        amount: 180.00
+                    }
+                ],
+                amount: null
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 350.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 70.00
+            },
+        ]
+    },
+    {
+        id: 6,
+        model: 'iPad Pro 9.7',
+        category: 2,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: [
+                    {
+                        title: 'GOOD TOUCH',
+                        amount: 45.00
+                    },
+                    {
+                        title: 'FAULTY TOUCH',
+                        amount: 55.00
+                    }
+                ],
+                amount: null
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 110.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 35.00
+            },
+        ]
+    },
+    {
+        id: 7,
+        model: 'iPad Pro 10.5',
+        category: 2,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: [
+                    {
+                        title: 'GOOD TOUCH',
+                        amount: 50.00
+                    },
+                    {
+                        title: 'FAULTY TOUCH',
+                        amount: 65.00
+                    }
+                ],
+                amount: null
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 150.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 35.00
+            },
+        ]
+    },
+    {
+        id: 8,
+        model: 'iPad Pro 12.9 1st Gen',
+        category: 2,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: [
+                    {
+                        title: 'GOOD TOUCH',
+                        amount: 80.00
+                    },
+                    {
+                        title: 'FAULTY TOUCH',
+                        amount: 110.00
+                    }
+                ],
+                amount: null
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 180.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 40.00
+            },
+        ]
+    },
+    {
+        id: 9,
+        model: 'iPad Pro 12.9 2nd Gen',
+        category: 2,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: [
+                    {
+                        title: 'GOOD TOUCH',
+                        amount: 130.00
+                    },
+                    {
+                        title: 'FAULTY TOUCH',
+                        amount: 180.00
+                    }
+                ],
+                amount: null
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 290.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 70.00
+            },
+        ]
+    },
+    {
+        id: 10,
+        model: 'iPad Pro 12.9 3rd Gen',
+        category: 2,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: [
+                    {
+                        title: 'GOOD TOUCH',
+                        amount: 150.00
+                    },
+                    {
+                        title: 'FAULTY TOUCH',
+                        amount: 180.00
+                    }
+                ],
+                amount: null
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 360.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 80.00
+            },
+        ]
+    },
+    {
+        id: 11,
+        model: 'iPad Pro 11',
+        category: 2,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: [
+                    {
+                        title: 'GOOD TOUCH',
+                        amount: 100.00
+                    },
+                    {
+                        title: 'FAULTY TOUCH',
+                        amount: 140.00
+                    }
+                ],
+                amount: null
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 360.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 60.00
+            },
+        ]
+    },
+    {
+        id: 12,
+        model: 'iPad Pro 11 2nd Gen',
+        category: 2,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: [
+                    {
+                        title: 'GOOD TOUCH',
+                        amount: 100.00
+                    },
+                    {
+                        title: 'FAULTY TOUCH',
+                        amount: 140.00
+                    }
+                ],
+                amount: null
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 360.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 60.00
+            },
+        ]
+    },
+    {
+        id: 13,
+        model: 'iPad Pro 12.9 4th Gen',
+        category: 2,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: [
+                    {
+                        title: 'GOOD TOUCH',
+                        amount: 150.00
+                    },
+                    {
+                        title: 'FAULTY TOUCH',
+                        amount: 180.00
+                    }
+                ],
+                amount: null
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 360.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 80.00
+            },
+        ]
+    },
+    {
+        id: 14,
+        model: 'Apple Watch S1 38MM / 42mm',
+        category: 2,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: null,
+                amount: 50.00
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 70.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 5.00
+            },
+        ]
+    },
+    {
+        id: 15,
+        model: 'Apple Watch S2 38MM / 42mm',
+        category: 2,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: null,
+                amount: 60.00
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 110.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 20.00
+            },
+        ]
+    },
+    {
+        id: 16,
+        model: 'Apple Watch S3 38MM / 42mm',
+        category: 2,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: null,
+                amount: 60.00
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 110.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 20.00
+            },
+        ]
+    },
+    {
+        id: 17,
+        model: 'Apple Watch S4 40MM / 42mm',
+        category: 2,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: null,
+                amount: 70.00
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 180.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 30.00
+            },
+        ]
+    },
+    {
+        id: 18,
+        model: 'Apple Watch S5 40MM / 42mm',
+        category: 2,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: null,
+                amount: 70.00
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 180.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 50.00
+            },
+        ]
+    },
+    {
+        id: 19,
+        model: 'Apple Watch S6 40MM / 42mm',
+        category: 2,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: null,
+                amount: 80.00
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 180.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 60.00
+            },
+        ]
+    },
+    {
+        id: 20,
+        model: 'Apple Watch SE 40MM / 42mm',
+        category: 2,
+        repairs: [
+            {
+                key: 'refurbishing',
+                title: 'Refurbishing - LCD MUST WORK',
+                children: null,
+                amount: 70.00
+            },
+            {
+                key: 'replacement',
+                title: "Replacement Price",
+                children: null,
+                amount: 180.00
+            },
+            {
+                key: 'broken-buy',
+                title: "Broken Buy Back",
+                children: null,
+                amount: 50.99
+            },
+        ]
+    }
+];
+
+$(function () {
+
+    loadTableData();
+    loadPreset(true);
+
+});
+
+const loadTableData = () => {
+
+    $.each(tableCartData, function (index, value) {
+
+        var prices = [];
+
+        $.each(value.repairs, function (i, valuePrice) {
+
+            if (valuePrice.children) {
+
+                $.each(valuePrice.children, function (j, childPrice) {
+
+                    prices.push(`<td class="text-center text-td-hover text-amount-calc">${childPrice.amount ? `<a onclick='calcRowTotal(this)'>&pound;${childPrice.amount.toFixed(2)} </a>` : "-"}</td>`);
+
+                });
+
+            }
+
+            if (valuePrice.amount && valuePrice.key === 'refurbishing') {
+                prices.push(`<td class="text-center text-td-hover text-amount-calc" colspan="${!valuePrice.children ? 2 : 0}">${valuePrice.amount && `<a onclick='calcRowTotal(this)'>&pound;${valuePrice.amount.toFixed(2)} </a>`}</td>`);
+            }
+            else if (valuePrice.amount) {
+                prices.push(`<td class="text-center text-td-hover text-amount-calc">${valuePrice.amount && `<a onclick='calcRowTotal(this)'>&pound;${valuePrice.amount.toFixed(2)}</a>`}</td>`);
+            }
+            else if (!valuePrice.children && !valuePrice.amount) {
+                prices.push('<td class="text-center">-</td>');
+            }
+
+        });
+
+        var item = `<tr>
+        <td class="text-center">${index + 1}</td>
+            <td>${value.model}</td>
+            ${prices.join('\n')}
+            <td class="text-center text-amount-calc-total">&pound;0.00</td>
+            </tr>`;
+
+        $("table#cart-table>tbody").append(item);
+
+    });
+
+}
+
+function string_to_slug(str) {
+    str = str.replace(/^\s+|\s+$/g, ''); // trim
+    str = str.toLowerCase();
+
+    // remove accents, swap ñ for n, etc
+    var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+    var to = "aaaaeeeeiiiioooouuuunc------";
+    for (var i = 0, l = from.length; i < l; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
+
+    str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+        .replace(/\s+/g, '-') // collapse whitespace and replace by -
+        .replace(/-+/g, '-'); // collapse dashes
+
+    return str;
+}
+
+function loadPreset(value) {
+    $("button#continue-navigation").prop("disabled", value);
+}
+
+function calcRowTotal(obj) {
+
+    if (hasClass(obj.parentElement, 'selected')) {
+        return toggleSelectedItem(obj);
+    }
+
+    unSelectedItem(obj.parentElement.parentNode);
+    totalAmountChange(obj.parentElement.parentNode, obj.innerText);
+    selectedItem(obj.parentElement);
+}
+
+function totalAmountChange(obj, total) {
+    obj.querySelector('td.text-amount-calc-total').innerText = total;
+
+    sumOfTotal();
+}
+
+function toggleSelectedItem(obj) {
+    totalAmountChange(obj.parentElement.parentNode, "£0.00");
+    obj.parentElement.classList.remove('selected');
+    console.log(obj.parentElement);
+}
+
+function hasClass(element, className) {
+    return (' ' + element.className + ' ').indexOf(' ' + className + ' ') > -1;
+}
+
+function selectedItem(obj) {
+    obj.classList.add('selected');
+}
+
+function unSelectedItem(obj) {
+    let lists = obj.querySelectorAll('td.text-amount-calc');
+
+    lists.forEach(element => {
+        element.classList.remove('selected');
+    });
+}
+
+function sumOfTotal() {
+
+    let lists = document.querySelectorAll('td.text-amount-calc-total');
+
+    var sum = 0.00;
+
+    lists.forEach(element => {
+        sum += parseFloat(element.innerText.replace(/^\D+/g, ''));
+    });
+
+    if (sum > 0) {
+        loadPreset(false);
+    }
+
+    document.querySelector('.cart-total-pricing b#cart-grand-total').innerText = "£" + sum.toFixed(2);
+
+}
+
+$("button#continue-navigation").click(function () {
+    $(location).attr('href', 'cart.html');
+});
+
+$("button#continue-navigation-back").click(function () {
+    $(location).attr('href', '/');
+});
